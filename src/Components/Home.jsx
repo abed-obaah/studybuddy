b3bcfc86a07ee63ef164c5db1b3968c1024a2fd8
Abed chinedu <chineduobaah@gmail.com>
2023-11-18 18:09:12 +0100

    live chat added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -390,7 +390,10 @@ export default function Example() {
                   href="#"
                   className="rounded-md bg-Primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
                 >
-                  Get started
+                  <Link to="/Register">
+                      Get started
+                  </Link>
+                 
                 </a>
                 <a href="#" className="flex items-center text-sm font-semibold leading-6 text-gray-900">
                  <span className=' text-3xl mr-2'><FaRegCirclePlay /> </span> How it works <span aria-hidden="true">→</span>
@@ -661,14 +664,15 @@ export default function Example() {
               <label htmlFor="email-address" className="sr-only">
                 Email address
               </label>
-              <input
-                id="email-address"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
-                placeholder="Enter your email"
+                <input
+                    id="email-address"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
+                    placeholder="Enter your email"
+                    style={{ '::placeholder': { color: 'text-white' } }}
               />
               <button
                 type="submit"
