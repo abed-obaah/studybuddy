ddf63ea62ff85eea7737b7b1f301e613f582e132
abednego obaah <96961755+abed-obaah@users.noreply.github.com>
2024-06-12 01:58:39 +0100

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -715,7 +715,7 @@ export default function Example() {
         <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
           <div className="xl:grid xl:grid-cols-3 xl:gap-8">
             <img
-              className="h-7"
+              className="h-10"
               src={footLogo}
               alt="Company name"
             />
